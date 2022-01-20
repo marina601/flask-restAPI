91a492f8b7acb8d1a06a53cc381f3150bd2480f2
marina601 <marina601@live.com>
2022-01-20 16:22:59 +0100

    Passed image src parametars to the update form

--- static/js/script.js
+++ static/js/script.js
@@ -12,15 +12,18 @@ function showPosts() {
             posts += `
             <div class="col-12 col-md-6 col-lg-4">
               <div class="card bg-light mb-3">
-                <img src="${post.imageUrl}" class="card-img-top card-image" alt="${post.title}">
                 <div class="card-body" data-id=${post._id.$oid}>
-                  <h5 class="card-title">Name: ${post.title}</h5>
-                  <p class="card-text card-content">Content: ${post.content}</p>
-                  <p class="card-text text-muted">Date: ${post.createdAt}</p>
-                  <p class='card-text text-muted card-author'>Creator: ${post.creator}</p>
+                  <img src="${post.imageUrl}" class="card-img-top card-image" id='card-image' alt="${post.title}">
+                  <h3 class="card-title">${post.title}</h3>
+                  <p class="card-text card-content">${post.content}</p>
+                  <span class='d-inline p-2'>Date:</span>
+                  <p class="card-text text-muted d-inline p-2">${post.createdAt}</p>
+                  <span class='d-inline p-2'>Author:</span>
+                  <p class='card-text text-muted card-author d-inline'>${post.creator}</p>
+                  <hr>
                   <button type="button" class="btn btn-warning" id='edit-post' data-toggle="modal" data-target="#exampleModal">Edit Post</button>
                   <a class='btn btn-danger' href='#' id='delete-post'>Delete</a>
-                  <a class='btn btn-info' href='#'>View</a>
+                  <button type="button" class="btn btn-info" id='view-post' data-toggle="modal" data-target="#viewModal">View Post</button>
                 </div>
               </div>
             </div>`;
@@ -31,29 +34,6 @@ function showPosts() {
 showPosts();
 
 
-//PUT
-//View individual post 
-document.getElementById('formData').addEventListener('submit', retriveName);
-// get the data by id
-function retriveName (e) {
-    e.preventDefault();
-
-    let id = document.getElementById('name').value;
-
-    fetch(`http://localhost:4444/${id}`)
-    .then((res) => res.json())
-    .then((data) => {
-        document.getElementById("output").innerHTML = `
-        <div class="card" style="width: 18rem;">
-          <img src="${data.imageUrl}" class="card-img-top" alt="...">
-           <div class="card-body">
-            <h5 class="card-title">Name: ${data.title}</h5>
-            <p class="card-text">Content: ${data.content}</p>
-            <p class="card-text text-muted">Date: ${data.createdAt}</p>
-            <p class='card-text text-muted'>Creator: ${data.creator}</p>
-        </div>`;
-    });
-};
 
 // POST
 // Create a new post 
@@ -87,6 +67,7 @@ function postData (e) {
         dataArr.push(data);
         // call show post function with update content
         showPosts(dataArr);
+        location.reload()
         alert('New Post Created!')
     })
 }
@@ -95,7 +76,7 @@ function postData (e) {
 let from_creator = document.getElementById('creator');
 let form_title = document.getElementById('title');
 let form_content = document.getElementById('content');
-let form_imageUrl = document.getElementById('imageUrl').value;
+let form_imageUrl = document.getElementById('imageUrl');
 // DELETE
 // target the div where the list of posts is located
 const postList = document.getElementById('posts');
@@ -106,6 +87,7 @@ postList.addEventListener('click', (e) => {
     e.preventDefault();
     let delBtn = e.target.id == 'delete-post';
     let editBtn = e.target.id == 'edit-post';
+    let viewBtn = e.target.id == 'view-post';
 
     let id = e.target.parentElement.dataset.id;
     console.log(id)
@@ -127,17 +109,17 @@ postList.addEventListener('click', (e) => {
         let title = parent.querySelector('.card-title').textContent;
         let content = parent.querySelector('.card-content').textContent;
         let creator = parent.querySelector('.card-author').textContent;
-        console.log(creator);
+        let imageUrl = parent.querySelector('.card-image').src;
+
+        console.log(imageUrl);
 
         
 
         form_title.value = title;
         form_content.value = content;
         from_creator.value = creator;
+        form_imageUrl.value = imageUrl; 
     }
-    
-    let today = new Date();
-    let date = today.getDate();
 
     const editButton = document.querySelector('.edit');
     // Update existing post
@@ -151,14 +133,50 @@ postList.addEventListener('click', (e) => {
             },
             body: JSON.stringify({'creator': creator,
                 'title': form_title.value,
-                'imageUrl': form_imageUrl,
+                'imageUrl': form_imageUrl.value,
                 'content': form_content.value ,
-                'createdAt': date,
                 'creator': from_creator.value,
             })
         })
           .then(res => res.json())
-          .then(() => location.reload())
+          .then(() =>  location.reload())
           alert('Post updated!')
     })
+
+    //GET
+    //View individual post 
+    if(viewBtn) {
+        fetch(url + id)
+        .then((res) => res.json())
+        .then((data) => {
+        document.getElementById("output").innerHTML = `
+        <div class="modal fade" id="viewModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                         <span aria-hidden="true">&times;</span>
+                       </button>
+                  </div>
+                <div class="modal-body">
+                   <div class="card" style="width: 100%;">
+                        <img src="${data.imageUrl}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h3 class="card-title">${data.title}</h3>
+                            <p class="card-text">${data.content}</p>
+                            <span class='d-inline p-2'>Updated on:</span>
+                            <p class="card-text text-muted d-inline p-2">${data.createdAt}</p>
+                            <span class='d-inline p-2'>Author:</span>
+                            <p class='card-text text-muted card-author d-inline'>${data.creator}</p>
+                            <hr>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+    });
+    }
 })
